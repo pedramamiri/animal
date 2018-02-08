f37c0f32f13547f452078b7bc7230a153dd712fc
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-02-08 21:07:36 +0100

    Update index.php

--- animal/index.php
+++ animal/index.php
@@ -1,34 +1,9 @@
+<?php session_start(); if($_SESSION){ header('Location: http://localhost:1337/result.php');}else{ header('Location: http://localhost:1337/settings.php'); }  ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Animal</title>
 </head>
 <body>
-<h1>Animal</h1>
-<form action="result.php" method="get">
-<input type="checkbox" name="anim1" value="apor">apor
-<br>
-<input type="number" name="quantity1" min="1" max="5">
-<br>
-<input type="checkbox" name="anim2" value="giraffer">giraffer
-<br>
-<input type="number" name="quantity2" min="1" max="5">
-<br>
-<input type="checkbox" name="anim3" value="tigrar">tigrar
-<br>
-<input type="number" name="quantity3" min="1" max="5"> 
-<br>
-<input type="checkbox" name="anim4" value="Kamel">kamel
-<br>
-<input type="number" name="quantity4" min="1" max="5"> 
-<br><br>
-<input type="submit">
-</form> 
-
-<?php
-
-
-?>
-
 </body>
 </html>
