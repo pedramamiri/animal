da062f981eab6b4f9acc911ef917800f2ab07ff3
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-02-08 21:16:11 +0100

    Update index.php

--- animal/index.php
+++ animal/index.php
@@ -1,4 +1,4 @@
-<?php session_start(); if($_SESSION){ header('Location: http://localhost:1337/result.php');}else{ header('Location: http://localhost:1337/settings.php'); }  ?>
+<?php session_start(); if($_SESSION){ header('Location: ./result.php');}else{ header('Location: ./settings.php'); }  ?>
 <!DOCTYPE html>
 <html>
 <head>
